b9ac3d583334a1173a04b8a9be9b44648a83d209
GCLemon <green.c.lemon@gmail.com>
2020-12-19 16:09:11 +0900

    Remove unused file

--- src/Wrapper.cpp
+++ src/Wrapper.cpp
@@ -1,4 +1,27 @@
-#include "Wrapper.h"
+#include <stdio.h>
+#include <stdint.h>
+
+#include "TLSFAllocator.h"
+
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#include <Windows.h>
+#endif
+
+#ifndef DLL_EXPORT
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#define DLL_EXPORT __declspec(dllexport)
+#else
+#define DLL_EXPORT
+#endif
+#endif
+
+#ifndef STDCALL
+#if defined(_WIN32) || defined(__WIN32__) || defined(__CYGWIN__)
+#define STDCALL __stdcall
+#else
+#define STDCALL
+#endif
+#endif
 
 extern "C"
 {
